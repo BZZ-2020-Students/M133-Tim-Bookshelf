80e9031fb9114c406a2d3d855ffd4fa9d3d3fcee
Zwazel <zwazel@hotmail.com>
2022-05-10 14:19:50 +0200

    property

--- src/main/java/ch/bzz/dev/zwazel/timbookshelf/Config.java
+++ src/main/java/ch/bzz/dev/zwazel/timbookshelf/Config.java
@@ -3,7 +3,35 @@ package ch.bzz.dev.zwazel.timbookshelf;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @ApplicationPath("/resource")
 public class Config extends Application {
+    private static Properties properties;
 
+    private static void checkIfPropertyExists() {
+        if (properties == null) {
+            properties = new Properties();
+            try (InputStream input = new FileInputStream(Config.class.getClassLoader().getResource("config.properties").getFile())) {
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        checkIfPropertyExists();
+
+        return properties.getProperty(key, defaultValue);
+    }
+
+    public static String getProperty(String key) {
+        checkIfPropertyExists();
+
+        return properties.getProperty(key);
+    }
 }
