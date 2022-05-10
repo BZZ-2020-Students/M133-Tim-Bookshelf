91f0cfd30e4adb9124b81d623aef7a512e2d280a
Zwazel <zwazel@hotmail.com>
2022-05-10 13:59:44 +0200

    service fertig

---
+++ src/main/java/ch/bzz/dev/zwazel/timbookshelf/service/TestService.java
@@ -0,0 +1,16 @@
+package ch.bzz.dev.zwazel.timbookshelf.service;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+@Path("test")
+public class TestService {
+    @GET
+    @Produces("text/plain")
+    @Path("test")
+    public Response test() {
+        return Response.status(200).entity("test erfolgreich").build();
+    }
+}
